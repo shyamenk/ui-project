995ffb4678923989d1cbeddb7b19b3a16f1998da
shyam <shyamenk@gmail.com>
2023-10-01 17:29:59 +0530

    feat: Initialize project with TypeScript, AntD, React Icons, and Tailwind CSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "antd/dist/reset.css";
+import { ConfigProvider } from "antd";
+import { themes } from "./theme/themes";
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider theme={themes}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
